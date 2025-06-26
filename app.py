8b06b4a52192036a9586b9335fe17331cc34d7a4
canh0t0 <109841828+canh0t0@users.noreply.github.com>
2025-06-26 16:57:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ import threading
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Sacerbot está rodando!", 200
+
 @app.route('/health')
 def health():
     return "Sacerbot Online", 200
 
 def run_bot():
     import main
-    if __name__ == "__main__":
-        main.bot.run(os.getenv('DISCORD_TOKEN'))
+    main.bot.run(os.getenv('DISCORD_TOKEN'))
 
 if __name__ == "__main__":
     bot_thread = threading.Thread(target=run_bot)
