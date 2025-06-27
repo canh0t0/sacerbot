df94b4d27cb74f5470b793ee3866b175dc27af42
canh0t0 <109841828+canh0t0@users.noreply.github.com>
2025-06-27 13:51:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import os
-import threading
 
 app = Flask(__name__)
 
@@ -12,11 +11,5 @@ def index():
 def health():
     return "Sacerbot Online", 200
 
-def run_bot():
-    import main
-    main.bot.run(os.getenv('DISCORD_TOKEN'))
-
 if __name__ == "__main__":
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 10000)))
